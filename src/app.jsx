35e64ddb5ba150d25dfdab66cc23b5a5a325f285
vogtn <nicolasjvogt@gmail.com>
2017-03-24 13:41:57 -0700

    fin

--- src/app.jsx
+++ src/app.jsx
@@ -3,14 +3,21 @@ const ReactDOM = require('react-dom');
 const YearBook = require('./components/YearBook');
 const PersonShow = require('./components/PersonShow');
 
+const Router = require('react-router').Router;
+const Route = require('react-router').Route;
+const browserHistory = require('react-router').browserHistory;
+
+
 const App = React.createClass({
   render: function() {
     return (
       <div>
-        <h1>Yearbook Component</h1>
-        <YearBook />
-        <h1>PersonShow Component (Hardcoded to Person 0)</h1>
-        <PersonShow />
+        <Router history={browserHistory}>
+          <h1>Yearbook Component</h1>
+          <Route path="/" component={YearBook} />
+          <h1>PersonShow Component (Hardcoded to Person 0)</h1>
+          <Route path="/show" component={PersonShow} />
+        </Router>
       </div>
     );
   }
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -9,10 +9,8 @@ const Person = React.createClass({
     return `${this.capitalize(name.first)} ${this.capitalize(name.last)}`
   },
   render: function() {
-    let picture = this.props.person.user.picture;
     return (
       <div className="col-md-3">
-        <img src={picture.medium} />
         <h3>{this.personName()}</h3>
       </div>
     );
